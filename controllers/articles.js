8e22af1429f967ef7e6e2aa792ee726cc9682d8a
weston-bailey <wdbailey87@gmail.com>
2021-02-21 11:04:26 -0800

    refactor controllers

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,6 +1,33 @@
 const router = require('express').Router()
 const db = require('../models')
 
-console.log('hello world from articles')
+// POST /articles - CREATE a new article
+router.post('/', async (req, res) => {
+  try {
+    const article = await db.article.create({
+      title: req.body.title,
+      content: req.body.content,
+      authorId: req.body.authorId
+    })
+    res.redirect(`/articles/${article.id}`)
+  } catch(error) {
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// GET /articles/:id - READ a specific article and include its author
+router.get('/:id', async (req, res) => {
+  try {
+    const article = await db.article.findOne({
+      where: { id: req.params.id },
+      include: [db.author]
+    })
+    res.json({ article: article })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
 
 module.exports = router
--- controllers/authors.js
+++ controllers/authors.js
@@ -1,6 +1,44 @@
 const router = require('express').Router()
 const db = require('../models')
 
-console.log('hello world from authors')
+// GET /authors - READ all authors
+router.get('/', async (req, res) => {
+  try {
+    const authors = await db.author.findAll()
+    res.json({ authors: authors })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// POST /authors - CREATE a new author
+router.post('/', async (req, res) => {
+  try {
+    const newAuthor = await db.author.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      bio: req.body.bio
+    })
+    res.redirect(`/authors/${newAuthor.id}`)
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// GET /authors/:id - READ a specific author and inlcude their posts
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await db.author.findOne({
+      where: {id: req.params.id},
+      include: [db.article]
+    })
+    res.json({ author: author })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
 
 module.exports = router
