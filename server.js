aa56c4a41fdb5f5be5db58c051f1d9332c177f74
weston-bailey <wdbailey87@gmail.com>
2021-02-20 22:44:00 -0800

    refactor to async routes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const morgan = require('morgan')
 
 // config express app
 const app = express()
+const PORT = 3000
 const rowdyResults = rowdy.begin(app)
-const port = 3000
 
 // express middlewares
 app.use(morgan('dev'))
@@ -18,15 +18,14 @@ app.use(express.urlencoded({ extended: false }))
  */
 
 // GET / - display all articles and their authors
-app.get('/', (req, res) => {
-  db.article.findAll({
-    include: [db.author]
-  }).then((articles) => {
-    res.render('main/index', { articles: articles })
-  }).catch((error) => {
+app.get('/', async (req, res) => {
+  try {
+    const articles = await db.article.findAll({ include: [db.author] })
+    res.json({ articles: articles })
+  } catch(error) {
     console.log(error)
-    res.status(400).render('main/404')
-  })
+    res.status(400).json({ message: 'bad request' })
+  }
 })
 
 /**
@@ -34,47 +33,43 @@ app.get('/', (req, res) => {
  */
 
 // GET /authors - display all authors
-app.get('/', (req, res) => {
-  db.author.findAll()
-  .then((authors) => {
-    res.render('authors/index', { authors: authors })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+app.get('/authors', async (req, res) => {
+  try {
+    const authors = await db.author.findAll()
+    res.json({ authors: authors })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
 })
 
 // POST /authors - create a new author
-app.post('/', (req, res) => {
-  db.author.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    bio: req.body.bio
-  })
-  .then((author) => {
+app.post('/authors', async (req, res) => {
+  try {
+    await db.author.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      bio: req.body.bio
+    })
     res.redirect('/authors')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
-})
-
-// GET /authors/new - display form for creating a new author
-app.get('/new', (req, res) => {
-  res.render('authors/new')
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
 })
 
 // GET /authors/:id - display a specific author and their posts
-app.get('/:id', (req, res) => {
-  db.author.findOne({
-    include: [db.article],
-    where: {id: req.params.id}
-  }).then((author) => {
-    res.render('authors/show', { author: author })
-  }).catch((error) => {
+app.get('/authors/:id', async (req, res) => {
+  try {
+    const author = await db.author.findOne({
+      where: {id: req.params.id},
+      include: [db.article]
+    })
+    res.json({ author: author })
+  } catch(error) {
     console.log(error)
-    res.status(400).render('main/404')
-  })
+    res.status(400).json({ message: 'bad request' })
+  }
 })
 
 /**
@@ -82,54 +77,35 @@ app.get('/:id', (req, res) => {
  */
 
 // POST /articles - create a new post
-app.post('/', (req, res) => {
-  db.article.create({
-    title: req.body.title,
-    content: req.body.content,
-    authorId: req.body.authorId
-  })
-  .then((post) => {
+app.post('/articles', async (req, res) => {
+  try {
+    await db.article.create({
+      title: req.body.title,
+      content: req.body.content,
+      authorId: req.body.authorId
+    })
     res.redirect('/')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
-})
-
-// GET /articles/new - display form for creating new articles
-app.get('/new', (req, res) => {
-  db.author.findAll()
-  .then((authors) => {
-    res.render('articles/new', { authors: authors })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+  } catch(error) {
+    res.status(400).json({ message: 'bad request' })
+  }
 })
 
 // GET /articles/:id - display a specific post and its author
-app.get('/:id', (req, res) => {
-  db.article.findOne({
-    where: { id: req.params.id },
-    include: [db.author]
-  })
-  .then((article) => {
-    if (!article) throw Error()
-    console.log(article.author)
-    res.render('articles/show', { article: article })
-  })
-  .catch((error) => {
+app.get('/articles/:id', async (req, res) => {
+  try {
+    const article = await db.article.findOne({
+      where: { id: req.params.id },
+      include: [db.author]
+    })
+    res.json({ article: article })
+  } catch(error) {
     console.log(error)
-    res.status(400).render('main/404')
-  })
+    res.status(400).json({ message: 'bad request' })
+  }
 })
 
-// bring in authors and articles controllers
-app.use('/authors', require('./controllers/authors'))
-app.use('/articles', require('./controllers/articles'))
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
   rowdyResults.print()
 })
 
