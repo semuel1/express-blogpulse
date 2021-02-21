d92fdfb6b2e19bae8f2f2c55c94bbe5f6643e92d
weston-bailey <wdbailey87@gmail.com>
2021-02-21 11:15:21 -0800

    stub comment route

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const db = require('../models')
 
-console.log('hello world from comments')
+// POST /comments - CREATE new comment
+router.post('/', async (req, res) => {
+  try {
+    res.json({ message: 'hello from comments' })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ messgae: 'OH NO! 💩 something isnt right'})
+  }
+})
 
 module.exports = router
