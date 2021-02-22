7205963a3c5e250d5e26910caca34776413f9d26
weston-bailey <wdbailey87@gmail.com>
2021-02-21 20:59:42 -0800

    adjust routes

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,20 +1,6 @@
 const router = require('express').Router()
 const db = require('../models')
 
-// POST /articles - CREATE a new article
-router.post('/', async (req, res) => {
-  try {
-    const article = await db.article.create({
-      title: req.body.title,
-      content: req.body.content,
-      authorId: req.body.authorId
-    })
-    res.redirect(`/articles/${article.id}`)
-  } catch(error) {
-    res.status(400).json({ message: 'bad request' })
-  }
-})
-
 // GET /articles/:id - READ a specific article and include its author
 router.get('/:id', async (req, res) => {
   try {
--- controllers/authors.js
+++ controllers/authors.js
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
   }
 })
 
-// GET /authors/:id - READ a specific author and inlcude their posts
+// GET /authors/:id - READ a specific author and inlcude their articles
 router.get('/:id', async (req, res) => {
   try {
     const author = await db.author.findOne({
@@ -35,6 +35,23 @@ router.get('/:id', async (req, res) => {
       include: [db.article]
     })
     res.json({ author: author })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// POST /authors/:id/articles - CREATE a new article associated with an author
+router.post('/:id/articles', async (req, res) => {
+  try {
+    const author = await db.author.findByPk(req.params.id, {include: db.article})
+    if(!author) throw new Error('author not found')
+    const article = await db.article.create({
+      title: req.body.title,
+      content: req.body.content,
+    })
+    await author.addArticle(article)
+    res.redirect(`/authors/${req.params.id}`)
   } catch(error) {
     console.log(error)
     res.status(400).json({ message: 'bad request' })
