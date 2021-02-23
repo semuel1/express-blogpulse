de6064982949ac599eb6c4d298aa6697db0b79f5
semuel1 <semuelst1@gmail.com>
2021-02-22 16:48:20 -0800

    update

--- models/article.js
+++ models/article.js
@@ -1,20 +1,38 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class article extends Model {
-    static associate(models) {
-      models.article.belongsTo(models.author)
-    }
-  };
-  article.init({
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    authorId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'article',
-  });
-  return article;
-};
+const router = require('express').Router()
+const db = require('../models')
+
+/**
+ * /articles routes
+ */
+
+// GET /articles/:id - READ a specific post and include its author
+router.get('/:id', async (req, res) => {
+  try {
+    const article = await db.article.findOne({
+      where: { id: req.params.id },
+      include: [db.author, db.comment]
+    })
+    res.json({ article: article })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// POST /articles/:id/comments - CREATE a new comment
+router.post('/:id/comments', async (req, res) => {
+  try {
+    const article = await db.article.findByPk(req.params.id)
+    const newComment = await article.createComment({
+      name: req.body.name,
+      content: req.body.content
+    })
+
+    res.redirect(`/articles/${req.params.id}`)
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+module.exports = router
--- models/author.js
+++ models/author.js
@@ -1,24 +1,65 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class author extends Model {
-    static associate(models) {
-      models.author.hasMany(models.article)
-    }
+const router = require('express').Router()
+const db = require('../models')
 
-    getFullName() {
-      return this.firstName + this.lastName
-    }
-  };
-  author.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    bio: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'author',
-  });
-  return author;
-};
+/**
+ * /authors routes
+ */
+
+// GET /authors - READ all authors
+router.get('/', async (req, res) => {
+  try {
+    const authors = await db.author.findAll()
+    res.json({ authors: authors })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// POST /authors - CREATE a new author
+router.post('/', async (req, res) => {
+  try {
+    const newAuthor = await db.author.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      bio: req.body.bio
+    })
+    res.redirect(`/authors/${newAuthor.id}`)
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// GET /authors/:id - READ a specific author and inlcude their articles
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await db.author.findOne({
+      where: {id: req.params.id},
+      include: [db.article]
+    })
+    res.json({ author: author })
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+// POST /authors/:id/articles - CREATE a new article associated with an author
+router.post('/:id/articles', async (req, res) => {
+  try {
+    const author = await db.author.findByPk(req.params.id, {include: db.article})
+    if(!author) throw new Error('author not found')
+    const article = await db.article.create({
+      title: req.body.title,
+      content: req.body.content,
+    })
+    await author.addArticle(article)
+    res.redirect(`/authors/${req.params.id}`)
+  } catch(error) {
+    console.log(error)
+    res.status(400).json({ message: 'bad request' })
+  }
+})
+
+module.exports = router
